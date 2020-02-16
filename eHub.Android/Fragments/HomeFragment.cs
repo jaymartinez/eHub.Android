7022cf9253d16febc5b62c1eae4c92e4e6fd4839
BuildTools <unconfigured@null.spigotmc.org>
2020-02-16 10:46:15 -0800

    declare some vars

--- eHub.Android/Fragments/HomeFragment.cs
+++ eHub.Android/Fragments/HomeFragment.cs
@@ -1,12 +1,21 @@
 ﻿using Android.OS;
 using Android.Support.V7.App;
 using Android.Views;
+using Android.Widget;
 using Fragment = Android.Support.V4.App.Fragment;
 
 namespace eHub.Android.Fragments
 {
     public class HomeFragment : Fragment
     {
+        TextView _poolStatusLbl;
+        TextView _spaStatusLbl;
+        TextView _boosterStatusLbl;
+        TextView _heaterStatusLbl;
+
+        ImageView _poolLightStatusBulb; 
+        ImageView _spaLightStatusBulb; 
+        ImageView _groundLightStatusBulb; 
 
         public override void OnCreate(Bundle savedInstanceState)
         {
@@ -23,7 +32,7 @@ namespace eHub.Android.Fragments
             var act = Activity as AppCompatActivity;
             var ab = act.SupportActionBar;
             ab.Title = "Home";
-            
+
             base.OnViewCreated(view, savedInstanceState);
         }
     }
