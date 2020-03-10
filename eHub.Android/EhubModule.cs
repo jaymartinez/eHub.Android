f569caf9bf5887c8305933e29807c183b3c62d87
Jay Martinez <chicoav@gmail.com>
2020-03-09 22:10:11 -0700

    version bump

--- eHub.Android/EhubModule.cs
+++ eHub.Android/EhubModule.cs
@@ -20,7 +20,7 @@ namespace eHub.Android
             builder.Register(ctx =>
             {
                 //TODO read from manifest
-                return new AppVersion { VersionName = "1.2.1", VersionNumber = 1214 }; 
+                return new AppVersion { VersionName = "1.3.2", VersionNumber = 1320 }; 
             }).As<AppVersion>();
 
             builder.Register(ctx =>
